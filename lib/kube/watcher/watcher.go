7030fc38a49c31b34aaca931f186965e276cc78b
summeree22 <90389541+summeree22@users.noreply.github.com>
2025-08-02 17:46:39 +0900

    feat: Add KubernetestResource struct

--- lib/kube/watcher/watcher.go
+++ lib/kube/watcher/watcher.go
@@ -2,10 +2,12 @@ package watcher
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/gravitational/teleport/api/types"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -13,6 +15,11 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
+
+type PodResourceWithLabels struct {
+	types.KubernetesResource
+	Labels map[string]string `json:"labels,omitempty"`
+}
 
 func StartPodWatcher(ctx context.Context, webhookURL string) error {
 	log.Println("Waiting for Teleport to be ready...")
@@ -55,20 +62,39 @@ func StartPodWatcher(ctx context.Context, webhookURL string) error {
 		namespace := pod.GetNamespace()
 		labels := pod.GetLabels()
 
+		kubernetesResource := types.KubernetesResource{
+			Kind:      "pods",
+			Name:      podName,
+			Namespace: namespace,
+		}
+
+		PodResourceWithLabels := PodResourceWithLabels{
+			KubernetesResource: kubernetesResource,
+			Labels:             labels,
+		}
+
+		resourceJSON, err := json.MarshalIndent(PodResourceWithLabels, "", " ")
+		if err != nil {
+			log.Printf("Failed to marshal PodResourceWithLabels: %v", err)
+			continue
+		}
+
 		labelStr := ""
 		for k, v := range labels {
 			labelStr += fmt.Sprintf("%s=%s ", k, v)
 		}
 
 		message := fmt.Sprintf(
-			"*[%s]* Pod: `%s`\nNamespace: `%s`\nLabels: `%s`\nTime: %s",
-			eventType, podName, namespace, labelStr, time.Now().Format(time.RFC3339),
+			"*[%s]* PodResourceWithLabels:\n```json\n%s\n```\nTime: %s",
+			eventType, string(resourceJSON), time.Now().Format(time.RFC3339),
 		)
 
-		err := sendSlackNotification(webhookURL, message)
+		err = sendSlackNotification(webhookURL, message)
 		if err != nil {
 			log.Printf("Failed to send Slack notification: %v", err)
 		}
+
+		log.Printf("Pod event: %s - %s/%s with labels: %v", eventType, namespace, podName, labels)
 	}
 
 	return nil
